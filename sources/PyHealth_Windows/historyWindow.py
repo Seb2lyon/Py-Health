fe689b9094c29d38b05e4db483a1b03bcbb40dd1
Seb2lyon <seb2lyon.info@free.fr>
2020-08-14 15:41:55 +0200

    Place the dates in History window

--- sources/PyHealth_Windows/historyWindow.py
+++ sources/PyHealth_Windows/historyWindow.py
@@ -23,9 +23,31 @@ class historyWindow(Toplevel):
 		self.imageRedLabel = PhotoImage(file="images/RedLabel.gif")
 		self.imageOrangeLabel = PhotoImage(file="images/OrangeLabel.gif")
 
-		self.myCanvas = Canvas(self, bg='#E3FBFA', height=400, width=700)	
-
+		self.myCanvas = Canvas(self, bg='#E3FBFA', height=400, width=700)
 		self.myCanvas.create_image(15, 15, anchor=NW, image=self.imageGrid)
+
+		self.labelDates = []
+
+		self.labelDate1 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate1)
+		self.labelDate2 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate2)
+		self.labelDate3 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate3)
+		self.labelDate4 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate4)
+		self.labelDate5 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate5)
+		self.labelDate6 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate6)
+		self.labelDate7 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate7)
+		self.labelDate8 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate8)
+		self.labelDate9 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)
+		self.labelDates.append(self.labelDate9)
+		self.labelDate10 = Label(self, bg='#E3FBFA', height=1, width=10, font=self.mainPage.gridNormalFont)	
+		self.labelDates.append(self.labelDate10)
 
 		self.myCanvas.create_image(15, 288, anchor=NW, image=self.imageRedLabel)
 		self.myCanvas.create_image(15, 312, anchor=NW, image=self.imageOrangeLabel)
@@ -42,7 +64,16 @@ class historyWindow(Toplevel):
 		self.buttonClose['fg'] = "#FFFFFF"
 
 		self.myCanvas.place(x=0, y=0)
-
+		self.labelDate1.place(x=15, y=255)
+		self.labelDate2.place(x=82, y=255)
+		self.labelDate3.place(x=149, y=255)
+		self.labelDate4.place(x=216, y=255)
+		self.labelDate5.place(x=283, y=255)
+		self.labelDate6.place(x=350, y=255)
+		self.labelDate7.place(x=417, y=255)
+		self.labelDate8.place(x=484, y=255)
+		self.labelDate9.place(x=551, y=255)
+		self.labelDate10.place(x=618, y=255)
 		self.buttonClose.place(x=550, y=330)
 
 	def closeWindow(self):
